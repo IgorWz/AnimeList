de978fcb51613cc5cc53eaa5d92d6bac9c58707e
Igor Menezes <igormenezesrosario01@gmail.com>
2023-05-21 04:03:11 -0300

    Project Created

---
+++ src/main/java/com/listanimes/animelist/AnimelistApplication.java
@@ -0,0 +1,13 @@
+package com.listanimes.animelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimelistApplication.class, args);
+	}
+
+}
